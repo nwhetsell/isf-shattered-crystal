0d73512cb671ebdce545fee59573ce203f05b8f8
Nate Whetsell <nathan.whetsell@gmail.com>
2026-03-07 17:16:40 -0500

    Refactor

--- Shattered Crystal.fs
+++ Shattered Crystal.fs
@@ -7,32 +7,67 @@
     "INPUTS": [
         {
             "NAME": "objectColor",
-            "LABEL": "Object color",
+            "LABEL": "Object Color",
             "TYPE": "color",
             "DEFAULT": [0.5, 0.5, 0.8, 1]
+        },
+        {
+            "NAME": "backgroundColor",
+            "LABEL": "Background Color",
+            "TYPE": "color",
+            "DEFAULT": [0.3, 0.45, 0.7, 1]
+        },
+        {
+            "NAME": "baseSubsurfaceColor",
+            "LABEL": "Base Subsurface Color",
+            "TYPE": "color",
+            "DEFAULT": [0.5, 0.4, 0.6, 1]
+        },
+        {
+            "NAME": "objectSize",
+            "LABEL": "Box Size",
+            "TYPE": "float",
+            "DEFAULT": 2,
+            "MAX": 100,
+            "MIN": 0
+        },
+        {
+            "NAME": "subsurfaceSize",
+            "LABEL": "Subsurface Size",
+            "TYPE": "float",
+            "DEFAULT": 1.2,
+            "MAX": 100,
+            "MIN": 0
         }
     ],
     "ISFVSN": "2"
 }*/
 
-#define rot(a) mat2(cos(a),sin(a),-sin(a),cos(a))
+// Functions from LYGIA <https://github.com/patriciogonzalezvivo/lygia>
 
-// cube SDF, inlined
-float bx(vec3 p, vec3 s)
-{
-    vec3 q = abs(p) - s;
-    return min(max(q.x, max(q.y, q.z)), 0.) + length(max(q, 0.));
+// https://github.com/patriciogonzalezvivo/lygia/blob/main/math/rotate2d.glsl
+mat2 rotate2d(const in float r) {
+    float c = cos(r);
+    float s = sin(r);
+    return mat2(c, s, -s, c);
 }
 
+// https://github.com/patriciogonzalezvivo/lygia/blob/main/sdf/boxSDF.glsl
+float boxSDF( vec3 p, vec3 b ) {
+    vec3 d = abs(p) - b;
+    return min(max(d.x,max(d.y,d.z)),0.0) + length(max(d,0.0));
+}
+
+
 // "shatter" function - subtracts a bunch of semi-random planes from the object
-float sh(vec3 p, float d, float n, float a, float s, float o)
+float sh(vec3 p, float d, float a, float s, float o)
 {
     // loop for each plane
-	for (float i = 0.; i < n; i++) {
+	for (float i = 0.; i < 9.; i++) {
 	    // apply semi-random rotation
-		p.xy *= rot(a);
-		p.xz *= rot(a * 0.5);
-		p.yz *= rot(a + a);
+		p.xy *= rotate2d(a);
+		p.xz *= rotate2d(a * 0.5);
+		p.yz *= rotate2d(a + a);
 		// pick semi-random axis for plane
 		float c = mod(i, 3.) == 0. ? p.x : mod(i, 3.) == 1. ? p.y : p.z;
 		// subtract plane from object, using onioning and offset
@@ -45,30 +80,30 @@ float sh(vec3 p, float d, float n, float a, float s, float o)
 
 // scene/map function
 float time = 0.;
-vec3 subsurfaceColor = vec3(0);
-vec3 glowColor = vec3(0);
+vec4 subsurfaceColor = vec4(0, 0, 0, 1);
+vec4 glowColor = vec4(0, 0, 0, 0);
 float objectTransmission = 0.;
 float indexOfRefraction = 0.;
 float mp(vec3 p)
 {
     // rotate entire scene slowly
-	p.xz *= rot(time * 0.03 + 1.);
-	p.yz *= rot(time * 0.05 + 0.5);
+	p.xz *= rotate2d(time * 0.03 + 1.);
+	p.yz *= rotate2d(time * 0.05 + 0.5);
     // create 2 boxes - one is the actual object, one is purely used for my fake subsurface
-	float d = bx(p, vec3(2)) - 0.1;
-	float c = bx(p, vec3(1.2));
+	float d = boxSDF(p, vec3(objectSize)) - 0.1;
+	float c = boxSDF(p, vec3(subsurfaceSize));
     // shatter box
-	d = sh(p, d, 9., sin(time * 0.01 + 0.3) * 3., (cos(time * 0.1) * 0.5 + 0.5) * 0.5 + 0.008, 0.4);
+	d = sh(p, d, sin(time * 0.01 + 0.3) * 3., (cos(time * 0.1) * 0.5 + 0.5) * 0.5 + 0.008, 0.4);
     // set scene distance, add glow
 	float sceneDistance = d;
-	glowColor += 0.001 / (0.001 + d*d) * normalize(p*p) * 0.008;
+	glowColor.rgb += 0.001 / (0.001 + d*d) * normalize(p*p) * 0.008;
     // set object values - doing inside the scene allows for easier and nicer effects!
 	if (sceneDistance < 0.001) {
-		subsurfaceColor = pow(c, 3.) * vec3(0.5, 0.4, 0.6); // fake subsurface
+		subsurfaceColor = pow(c, 3.) * baseSubsurfaceColor; // fake subsurface
 		indexOfRefraction = 1.5 + c * 0.1; // index of refraction
 		objectTransmission = 0.8 - c * 0.2; // object transmission
 	}
-	return sceneDistance;// return the distance - this is only used for the normals function
+	return sceneDistance; // return the distance - this is only used for the normals function
 }
 
 // inlined raymarcher. Mostly standard, but multiplies the scene distance by the inversion factor
@@ -92,19 +127,22 @@ vec3 nm(vec3 currentRayPosition)
 }
 
 // pixel "shader" (coloury bits)
-vec3 px(vec3 currentRayPosition, vec3 currentRayNormal, vec3 currentRayDirection, float currentDistance)
+vec4 px(vec3 currentRayPosition, vec3 currentRayNormal, vec3 currentRayDirection, float currentDistance)
 {
-    vec3 currentColor = vec3(0.3, 0.45, 0.7) + length(currentRayDirection * currentRayDirection) * 0.2 + glowColor; // assign current color to background colour + glow
-    if (currentDistance > 16.)
-        return currentColor; // return if we just want background
-    // axis-based diffuse lighting
-    vec3 l = vec3(0.7, 0.4, 0.9);
-    float df = length(currentRayNormal * l);
-    // very basic fresnel effect and custom specular effect
-    float fr = pow(1. - df, 2.) * 0.5;
-    float sp = (1. - length(cross(currentRayDirection, currentRayNormal))) * 0.2;
-    float ao = min(mp(currentRayPosition + currentRayNormal * 0.5) - 0.5, 0.3) * 0.3; // custom ambient occulusion effect
-    return objectColor.rgb * (df + fr + subsurfaceColor) + fr + sp + ao + glowColor; // mix it all together
+    vec4 currentColor;
+    if (currentDistance > 16.) {
+        currentColor = backgroundColor + length(currentRayDirection * currentRayDirection) * 0.2 + glowColor; // assign current color to background colour + glow
+    } else {
+        // axis-based diffuse lighting
+        vec3 l = vec3(0.7, 0.4, 0.9);
+        float df = length(currentRayNormal * l);
+        // very basic fresnel effect and custom specular effect
+        float fr = pow(1. - df, 2.) * 0.5;
+        float sp = (1. - length(cross(currentRayDirection, currentRayNormal))) * 0.2;
+        float ao = min(mp(currentRayPosition + currentRayNormal * 0.5) - 0.5, 0.3) * 0.3; // custom ambient occulusion effect
+        currentColor = objectColor * (df + fr + subsurfaceColor) + fr + sp + ao + glowColor; // mix it all together
+    }
+    return currentColor;
 }
 
 void main()
@@ -121,7 +159,7 @@ void main()
 
     float currentTransmission = 1.;
     float transparencyInversion = 1.;
-    vec3 finalColor = vec3(0);
+    vec4 finalColor = vec4(0);
 
     for (int i = 0; i < 6 * 2; i++) { // compacted transparency loop - depth 6 (2 pass per layer)
         float currentDistance;
@@ -136,7 +174,7 @@ void main()
         if (length(originalRayDirection) == 0.)
             originalRayDirection = reflect(currentRayDirection, currentRayNormal * transparencyInversion); // reflect if refraction failed
 
-        vec3 currentColor = px(currentRayPosition, currentRayNormal, currentRayDirection, currentDistance);
+        vec4 currentColor = px(currentRayPosition, currentRayNormal, currentRayDirection, currentDistance);
 
         transparencyInversion *= -1.;
         if (transparencyInversion < 0.)
@@ -146,5 +184,5 @@ void main()
             break; // update trasmission and break if needed
     }
 
-    gl_FragColor = vec4(finalColor, 1); // output colour
+    gl_FragColor = finalColor; // output colour
 }
